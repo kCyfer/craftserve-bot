5ef5c1e6d8409aa3f7ab68e0ba76026879927e94
MinerPL <minerpl03@gmail.com>
2021-04-06 16:40:55 +0200

    Add mysql connection

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 ﻿const Discord = require('discord.js');
-const {token, prefix} = require('./config.json');
+const config = require('./config.json');
 const bot = new Discord.Client();
 
-bot.commands = new Discord.Collection()
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(config.mysql.database, config.mysql.user, config.mysql.password, {
+    host: config.mysql.host,
+    dialect: 'mysql',
+    logging: false,
+});
+
+bot.mysql = sequelize;
+bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-require("./commandHandler")(bot)
+require("./mysql")(bot);
+require("./commandHandler")(bot);
 require("./events/eventHandler")(bot);
 
 bot.on('message', message => {
     if (message.author.bot) {
         return;
     }
-    if (!message.content.startsWith(prefix)) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    if (!message.content.startsWith(config.prefix)) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
 
 
-    let command = bot.commands.get(cmd);
-    if(!command) command = bot.commands.get(bot.aliases.get(cmd));
+    const command = bot.commands.get(cmd) ?? bot.commands.get(bot.aliases.get(cmd));
 
     bot.embed = new Discord.MessageEmbed()
         .setColor(0x224d21)
         .setFooter(`Komenda !${cmd} | ${message.author.tag}`)
-        .setTimestamp()
+        .setTimestamp();
 
-    if(command) command.run(bot, args, message)
+    if(command) command.run(bot, args, message);
 });
 
-bot.login(token);
+bot.login(config.token);
