45f8a5bcaf740665e256bb14b854879a7936db7e
wojtek99019pl90 <83579930+wojtek99019pl90@users.noreply.github.com>
2021-08-04 22:55:29 +0200

    lepszy polski

--- commands/tab.js
+++ commands/tab.js
@@ -13,7 +13,7 @@ module.exports = {
     run: (bot, args, message) => {
         const embed = bot.embed
             .setTitle("Własny tab")
-            .setDescription('Jednym z najlepszych pluginów, który umożliwia zrobienie własnego taba, jest plugin **TAB**.\nMożesz go pobrać **[TUTAJ](https://github.com/NEZNAMY/TAB/releases/latest)**\nA **[tutaj](https://github.com/NEZNAMY/TAB/wiki)** znajdziesz wszelkie instrukcje.');
+            .setDescription('Jednym z najlepszych pluginów, który umożliwia zrobienie własnego taba, jest plugin **TAB**.\nMożesz go pobrać **[TUTAJ](https://github.com/NEZNAMY/TAB/releases/latest)**, a **[tutaj](https://github.com/NEZNAMY/TAB/wiki)** znajdziesz wszelkie instrukcje.');
 
         const text = args.join(" ");
 
