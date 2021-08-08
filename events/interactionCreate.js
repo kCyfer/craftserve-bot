f31f6fd10f5cab006b18c880ddcb03a62d65130d
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2021-08-08 05:41:06 +0200

    Add new command, event handlers, update discord.js version

---
+++ events/interactionCreate.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'interactionCreate',
+	async execute(interaction, client) {
+		if (!interaction.isCommand()) return;
+
+		if (!client.commands.has(interaction.commandName)) return;
+
+		try {
+			await client.commands.get(interaction.commandName).execute(interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'Wystąpił błąd podczas wykonywania tej komendy!', ephemeral: true });
+		}
+	},
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const permissions = require('./permissions.json');
+const config = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS] });
+client.commands = new Collection();
+
+fs.readdirSync('./commands/').forEach(dir => {
+	const commandFiles = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const command = require(`./commands/${dir}/${file}`);
+		command.category = dir;
+		if(!command.permissions) command.permissions = permissions[dir];
+		client.commands.set(command.name, command);
+	}
+});
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+
+
+client.login(config.token);
