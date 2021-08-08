abf9c465dd594ab2f0906454b0922a242639ed57
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2021-08-08 05:36:54 +0200

    Remove old things

--- events/eventHandler.js
+++
@@ -1,9 +0,0 @@
-const requestEvent = (event) => require(`./${event}`);
-
-module.exports = bot => {
-    bot.on("ready", async () => requestEvent("ready")(bot));
-    bot.on("ready", async () => requestEvent("createSlash")(bot));
-    bot.on("message", async (message) => requestEvent("antyInvite")(bot, message));
-    bot.on("message", async (message) => requestEvent("message")(bot, message));
-    bot.ws.on(`INTERACTION_CREATE`, (interaction) => requestEvent("slashcommands")(bot, interaction));
-};
