25ed6cce957fdcef1cd522bdd6da770fa18f1240
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2022-01-10 23:32:40 +0100

    Revert "Add feature to log used invite for new members"

--- events/eventHandler.js
+++ events/eventHandler.js
@@ -6,6 +6,5 @@ module.exports = bot => {
     bot.on("message", async (message) => requestEvent("antyInvite").message(bot, message));
     bot.on("messageUpdate", async (oldMessage, message) => requestEvent("antyInvite").edit(bot, oldMessage, message));
     bot.on("message", async (message) => requestEvent("message")(bot, message));
-    bot.on("guildMemberAdd", async (member) => requestEvent("guildMemberAdd")(bot, member));
     bot.ws.on(`INTERACTION_CREATE`, (interaction) => requestEvent("slashcommands")(bot, interaction));
 };
