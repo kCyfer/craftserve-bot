4c8bc91124e3f09511732d9e4a4bb5aeef2a8c4a
Adam <notimportantgoing@gmail.com>
2022-07-29 07:22:39 +0200

    Cosmetics of posted content

--- commands/secureprofile.js
+++ commands/secureprofile.js
@@ -13,7 +13,7 @@ module.exports = {
     run: (bot, args, message) => {
         const embed = bot.embed
             .setTitle("Problem z wejściem na serwer?")
-            .setDescription('Na wersjach od 1.19 może pojawić się komunikat `Invalid signature for profile public key` lub `Niewłaściwy podpis klucza publicznego profilu`.\n W celu rozwiązania problemu należy przejść do plików serwera i w pliku `server.properties` zmienić wartość `enforce-secure-profile` na `false` **NAWET** w sytuacji gdy serwer działa w tzw. offline mode (online-mode=false).')
+            .setDescription('Na wersjach od 1.19 może pojawić się komunikat:\n`Invalid signature for profile public key`\nlub\n`Niewłaściwy podpis klucza publicznego profilu`.\n\nW celu rozwiązania problemu należy przejść do plików serwera i w pliku `server.properties` zmienić wartość `enforce-secure-profile` na `false`\n\n**UWAGA!**\nWartość zmieniamy nawet gdy serwer działa w tzw. offline mode (online-mode=false).')
             .setImage(`https://cdn.discordapp.com/attachments/881557411264077867/1002368671827640320/unknown.png`)
 
         const text = args.join(" ");
