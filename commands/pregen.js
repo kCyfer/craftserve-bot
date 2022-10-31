fb8350844222452458858c191e0191c2ba57c543
MinerPL <minerpl03@gmail.com>
2022-10-31 23:29:38 +0100

    Move information about plugin description to new line

--- commands/pregen.js
+++ commands/pregen.js
@@ -13,7 +13,7 @@ module.exports = {
     run: (bot, args, message) => {
         const embed = bot.embed
             .setTitle("Pregen mapy")
-            .setDescription('Jednym z ważniejszych etapów robienia serwera jest wygenerowanie świata. Pomaga to m.in. w zmniejszeniu lagów. Aby to zrobić należy wgrać plugin [chunky](https://spigotmc.org/resources/chunky.81534/) oraz wpisać dwie komendy **/chunky radius <zasięg>** oraz **/chunky start**. Pod tym [linkiem](<https://github.com/pop4959/Chunky/wiki/Commands>) znajduje się dokładny opis pluginu.')
+            .setDescription('Jednym z ważniejszych etapów robienia serwera jest wygenerowanie świata. Pomaga to m.in. w zmniejszeniu lagów. Aby to zrobić należy wgrać plugin [chunky](https://spigotmc.org/resources/chunky.81534/) oraz wpisać dwie komendy **/chunky radius <zasięg>** oraz **/chunky start**. \nDokładny opis plugin można znaleźć pod tym [linkiem](<https://github.com/pop4959/Chunky/wiki/Commands>).')
 
 
         const text = args.join(" ");
